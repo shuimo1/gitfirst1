4220b9ab79f150ccd646a933b731a5110f5d7cd3
shuimo1 <guan1502351@163.com>
2017-07-12 09:38:13 +0800

    第一次

---
+++ src/com/test/GitDemo1.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by Administrator on 2017/7/12 0012.
+ */
+public class GitDemo1 {
+    public static void main(String[] args) {
+        System.out.println("pipixia");
+    }
+}
