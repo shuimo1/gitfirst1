2b37158ab8a66325c85026ff6c2d31bb0e1ec65a
shuimo1 <guan1502351@163.com>
2017-07-12 09:49:34 +0800

    userdao class second

--- src/com/test/GitDemo1.java
+++ src/com/test/GitDemo1.java
@@ -6,5 +6,6 @@ package com.test;
 public class GitDemo1 {
     public static void main(String[] args) {
         System.out.println("pipixia");
+
     }
 }
---
+++ src/com/test/UserDao.java
@@ -0,0 +1,8 @@
+package com.test;
+
+/**
+ * Created by Administrator on 2017/7/12 0012.
+ */
+public class UserDao {
+    void finduser(){}
+}
